b38cc2239b6a7f49d5fe5123e79e507f8890fde5
SoftwareEngineer <harry.patrick442@gmail.com>
2019-02-16 14:56:47 +0000

    finished my image preloader

---
+++ public/ImagePreloader.js
@@ -0,0 +1,68 @@
+var ImagePreloader = new (function(){
+	var self = this;
+	var preloaded={};
+	var mapUrlToPreloaderPreloading={};
+	this.preloadRange = function(urls, callback){
+		var rangePreloader;
+		each(urls, function(url){
+			if(preloaded[url])return;
+			var preloader = mapUrlToPreloaderPreloading[url];
+			if(!rangePreloader)rangePreloader = new RangePreloader({callback:callback});
+			rangePreloader.addPreloader(preloader?preloader:createPreloader(url));
+		});
+		if(!rangePreloader)
+			callback&&callback();
+			
+	};
+	function preloaderDone(e){
+		var preloader = e.preloader;
+		var url = preloader.getUrl();
+		delete mapUrlToPreloaderPreloading[url];
+		preloaded[url]=true;
+	}
+	function crceatePreloader(url){
+		var preloader = new Preloader(url);
+		mapUrlToPreloaderPreloading[url]=preloader;
+		return preloader;
+	}
+	function RangePreloader(params){
+		var callback = params.callback;
+		var preloaders =[];
+		this.addPreloader = function(){
+			preloader.addEventListener('done', done);
+			preloaders.push(preloader);
+		};
+		function done(e){
+			var preloader = e.preloader;
+			remove(preloader);
+			if(preloaders.length<1)
+				callback&&callback();
+		}
+		function remove(preloader){
+			var index = preloaders.indexOf(preloader);
+			if(index<0)return;
+			preloaders.splice(index, 1);
+		}
+	}
+	function Preloader(params){
+		EventEnabledBuilder(this);
+		var self = this;
+		var url = params.url;
+		var img = E.IMG();
+		var successful = false;
+		img.onload = function(){
+			successful = true;
+			dispatchDone();
+		};
+		img.onerror=function(){
+			dispatchDone();
+		};
+		img.src=url;
+		this.getSuccessful = function(){
+			return successful;
+		};
+		function dispatchDone(){
+			self.dispatchEvent({type:'done', preloader:self});
+		}
+	}
+})();
