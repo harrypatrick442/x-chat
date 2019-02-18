b25517ed9a90cebacca1d9f9cb0057e2bc7306bb
SoftwareEngineer <harry.patrick442@gmail.com>
2019-02-18 08:18:07 +0000

    implementing webrtc

--- public/ClientClientVideoAudio.js
+++ public/ClientClientVideoAudio.js
@@ -2,12 +2,40 @@ var ClientClientVideoAudio = new (function () {
 	var _ClientClientPortal = function(params){
 		EventEnabledBuilder(this);
 		var self = this;
+		var rtcPeerConnection;
 		this.start = function(callback){
 			getUserPermission(function(result){
 				(!result.successful)&&callback(result);
 				
 			});
 		};
+		function createNewPC() {
+			rtcPeerConnection = new RTCPeerConnection(null);
+			self.connected = true;//moved from inside onaddstream.
+			rtcPeerConnection.onaddstream = onAddStream;
+			rtcPeerConnection.onremovestream = onRemoveStream;
+			rtcPeerConnection.oniceconnectionstatechange = onIceConnectionStateChange;
+			rtcPeerConnection.onicecandidate = onIceCandidate;
+		}
+		function onAddStream(e){
+			var stream = e.stream;
+			attachMediaStream(remoteVideo, stream);
+			remoteStream = stream;
+		}
+		function onRemoveStream(e){
+			
+		}
+		function onIceConnectionStateChange(e){
+			if (pc.iceConnectionState == 'disconnected') {
+				
+			}
+		}
+		function onIceCandidate(e){
+			var candidate=e.candidate;
+			if (candidate != null) {
+				
+			}
+		}
 		function getUserPermission(callback){
 			var constraints =  {audio: true,  video: true};
 			navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
