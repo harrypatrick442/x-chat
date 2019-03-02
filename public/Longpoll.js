f75dd5494c3db9b14859acb8efd1335a2ab13fe7
SoftwareEngineer <harry.patrick442@gmail.com>
2019-03-02 23:20:00 +0000

    think its solved. testing on server

--- public/Longpoll.js
+++ public/Longpoll.js
@@ -4,28 +4,41 @@ var Longpoll = (function(){
 		var self = this;
 		var url = params.url;
 		var id = params.id;
+		console.log('incoming id is: '+id);
 		var ajax = new Ajax({url:url});
 		var stop=false;
 		var started=false;
-		this.send = function(msg){
+		var didFirstSend = false;
+		var waitingToBeSent = [];
+		this.send = function(msg){//issue was caused by multiple sends in parallel before an id got returned.
 			if(closed)return;
-			ajax.post({data:JSON.stringify({id:id, msg:msg}), callbackSuccessful:callbackSendSuccessful, callbackFailed:callbackSendError});
+			if(started||!didFirstSend){
+				didFirstSend = true;
+				ajax.post({data:JSON.stringify({id:id, msg:msg}), callbackSuccessful:callbackSendSuccessful, callbackFailed:callbackSendError});
+			}
+			else
+				waitingToBeSent.push(msg);
 		};
 		this.stop=function(){
 			stop=true;
 		};
 		this.dispose=this.stop;
 		function poll(){
+			console.log('get id is: '+id);
 			ajax.get({url:urlPoll, timeout:TIMEOUT, callbackSuccessful:callbackPollSuccessful, callbackFailed:callbackPollError, callbackTimeout:callbackPollTimeout});
 		}
 		function callbackSendSuccessful(res){
 			res = JSON.parse(res);
-			if(res.id)
-				id = res.id;
-			urlPoll = url+'/'+id;
-			dispatchGotId(id);
+			console.log(res.id);
 			if(started)return
 			started=true;
+			id = res.id;
+			urlPoll = url+'/'+id;
+			dispatchGotId(id);
+			each(waitingToBeSent, function(msg){
+				self.send(msg);
+			});
+			waitingToBeSent=null;
 			poll();
 		}
 		function callbackSendError(err){
