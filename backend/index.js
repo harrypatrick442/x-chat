17110fa60fb42accd440fd82b16f436e7e94a5f7
SoftwareEngineer <harry.patrick442@gmail.com>
2019-03-03 19:45:05 +0000

    using greenlock for ssl

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,45 @@
-const SIZE_LIMIT_MB=1.5;
-var dal = require('./dal');
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var path = require('path');
-var servlet = require('./servlet').servlet;
-var endpoint = require('./endpoint').endpoint;
-var endpointLongpoll = require('./EndpoingLongpoll');
-var imageUploader = new (require('./ImageUploader').ImageUploader)();
-app.use(bodyParser.json({ limit: String(SIZE_LIMIT_MB)+'mb' }));
-app.use(bodyParser.urlencoded({ limit: String(SIZE_LIMIT_MB)+'mb', extended: true, parameterLimit: 50000 }));
-servlet(app);
-endpoint(app);
-app.use(express.static(path.join(__dirname, '../public')));
-var server = app.listen(80, function () {
-    console.log('Server is running..');
-});
-server.setTimeout(5000, function(r){console.log('timed out'); //console.log(r);
-});
-app.post('/image_uploader', function(req, res){
-	console.log(req);
-	res.send(imageUploader.process(req));
-});
-endpointLongpoll.load(app);
+
+
+
+
+
+'use strict';
+
+require('greenlock-express').create({
+  email: 'awonderfulmachine@gmail.com'     // The email address of the ACME user / hosting provider
+, agreeTos: true                    // You must accept the ToS as the host which handles the certs
+, configDir: '~/.config/acme/'      // Writable directory where certs will be saved
+, communityMember: true             // Join the community to get notified of important updates
+, telemetry: true                   // Contribute telemetry data to the project
+
+  // Using your express app:
+  // simply export it as-is, then include it here
+, app: function(){
+		const SIZE_LIMIT_MB=1.5;
+		var dal = require('./dal');
+		var express = require('express');
+		var bodyParser = require('body-parser');
+		var app = express();
+		var path = require('path');
+		var servlet = require('./servlet').servlet;
+		var endpoint = require('./endpoint').endpoint;
+		var endpointLongpoll = require('./EndpoingLongpoll');
+		var imageUploader = new (require('./ImageUploader').ImageUploader)();
+		app.use(bodyParser.json({ limit: String(SIZE_LIMIT_MB)+'mb' }));
+		app.use(bodyParser.urlencoded({ limit: String(SIZE_LIMIT_MB)+'mb', extended: true, parameterLimit: 50000 }));
+		servlet(app);
+		endpoint(app);
+		app.use(express.static(path.join(__dirname, '../public')));
+		var server = app.listen(80, function () {
+			console.log('Server is running..');
+		});
+		server.setTimeout(5000, function(r){console.log('timed out'); //console.log(r);
+		});
+		app.post('/image_uploader', function(req, res){
+			console.log(req);
+			res.send(imageUploader.process(req));
+		});
+		endpointLongpoll.load(app);
+	}
+ debug: true
+}).listen(80, 443);
