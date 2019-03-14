cd6bd6050f98598e1a2284712ba43eee134760e7
SoftwareEngineer <harry.patrick442@gmail.com>
2019-03-14 08:14:10 +0000

    precompiler almost done.

--- precompilation/index.js
+++ precompilation/index.js
@@ -1,18 +1,53 @@
-var fs = require('fs');
-var path = require('path');
-var fs = require('fs');
-var each = require('./../backend/each');
-var publicFolder = path.join(__dirname , '../public');
-var precompiledFolder = path.join(__dirname, '../precompiled');
-var indexHtmlFile = path.join(publicFolder, 'index.html');
-var mIndexHtmlFile = path.join(publicFolder, 'm.index.html');
-var precompiledJsFile = path.join(precompiledFolder, '/index.js');
-concatenateScripts(indexHtmlFile, function(concatenatedScript){
-		//console.log(concatenatedScript);
-		writeFile(precompiledJsFile, concatenatedScript);
+const fs = require('fs');
+const path = require('path');
+const each = require('./../backend/each');
+const publicFolder = path.join(__dirname , '../public');
+const precompiledFolder = path.join(__dirname, '../precompiled');
+const indexHtmlFile = path.join(publicFolder, 'index.html');
+const mIndexHtmlFile = path.join(publicFolder, 'm.index.html');
+const precompiledJsFile = path.join(precompiledFolder, '/index_'+getUniqueString()+'.js');
+const precompiledMJsFile = path.join(precompiledFolder, '/m.index_'+getUniqueString()+'.js');
+const precompiledCssFile = path.join(precompiledFolder, '/styles_'+getUniqueString()+'.css');
+const precompiledMCssFile = path.join(precompiledFolder, '/m.styles_'+getUniqueString()+'.css');
+emptyFolder(precompiledFolder, function(){
+	concatenateScripts(indexHtmlFile, function(concatenatedScript){
+			writeFile(precompiledJsFile, concatenatedScript);
+			concatenateStyles(indexHtmlFile, function(concatenatedStyles){
+				writeFile(precompiledCssFile, concatenatedStyles);
+			})
+	});
+	concatenateScripts(mIndexHtmlFile, function(concatenatedScript){
+			//console.log(concatenatedScript);
+			writeFile(precompiledMJsFile, concatenatedScript);
+			concatenateStyles(mIndexHtmlFile, function(concatenatedStyles){
+				writeFile(precompiledMCssFile, concatenatedStyles);
+			})
+	});
 });
+function emptyFolder(directory, callback){
+	fs.readdir(directory, (err, files) => {
+	  if (err) throw err;
+	  var n = files.length;
+	  for (const file of files) {
+		fs.unlink(path.join(directory, file), err => {
+		  if (err) throw err;
+		  n--;
+		  if(n<=0)
+			  callback();
+		});
+	  }
+	});
+}
 function concatenateScripts(htmlFile, callback){
-	getScriptFilePathsFromHtml(htmlFile, function(scriptFilePaths){
+	var regExp = new RegExp('type *= *[\'|"]text/javascript[\'|"] *src *= *[\'|"](.+\.js)[\'|"] *> *</script *>','g');
+	concatenate(htmlFile, regExp, callback);
+}
+function concatenateStyles(htmlFile, callback){
+	var regExp = new RegExp('href *= *[\'|"](.+\.css).*\'> *</link *>','g');
+	concatenate(htmlFile, regExp, callback);
+}
+function concatenate(htmlFile, regExp, callback){
+	getFilePathsFromHtml(regExp, htmlFile, function(scriptFilePaths){
 		var concatenatedScript='';
 		var i=0;
 		function next(){
@@ -38,8 +73,7 @@ function writeFile(path, data){
 		}
 	}); 
 }
-function getScriptFilePathsFromHtml(filePath, callback){
-	var regExp = new RegExp('type *= *[\'|"]text/javascript[\'|"] *src *= *[\'|"](.+\.js)[\'|"] *> *</script *>','g');
+function getFilePathsFromHtml(regExp, filePath, callback){
 	readFile(filePath, function(data){
 			var list =[];
 			var m;
@@ -60,4 +94,7 @@ function readFile(filePath, callback){
 			console.log(err);
 	}
 	});
+}
+function getUniqueString(){
+	return String(new Date().getTime());
 }
