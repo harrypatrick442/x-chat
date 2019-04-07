790934d4f28d8a42d4e92a01ead517f803770163
SoftwareEngineer <harry.patrick442@gmail.com>
2019-04-07 19:10:46 +0100

    cookie warning remember acceptance.

--- public/CookieWarning.js
+++ public/CookieWarning.js
@@ -1,7 +1,9 @@
 (function(){
+	var settings = new Settings('cookieWarning');
+	if(settings.get('accepted'))return;
 	Dialog.show({message:"This site uses cookies to remember your username and various settings. By entering you accept this.",
 			buttons:[{text:'That\'s ok', callback:thatsOk}], classNames:['cookie-warning']
 	});
 	function thatsOk(){
-		
+		settings.set('accepted', true);
 	}})();
